06aeb0307446e3204750422ade149f173245cc23
JCotton1123 <jcotton1123@gmail.com>
2014-02-05 23:13:13 -0500

    Update all_in_one_app_server.pp

--- manifests/role/all_in_one_app_server.pp
+++ manifests/role/all_in_one_app_server.pp
@@ -1,6 +1,5 @@
 class bitlancerorg::role::all_in_one_app_server inherits bitlancerorg::role {
-  include stringed::profile::httpd::bare
-  include stringed::profile::phpfpm
+  include stringed::wrapper::apache_phpfpm
   include stringed::profile::mongodb
   include stringed::profile::rabbitmq
   include stringed::profile::redis
