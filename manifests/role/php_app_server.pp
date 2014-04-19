a4f2ccb65e34b5ca895c02a5142a83880a30736a
JCotton1123 <jcotton1123@gmail.com>
2014-04-19 10:34:03 -0400

    Update php_app_server.pp

--- manifests/role/php_app_server.pp
+++ manifests/role/php_app_server.pp
@@ -1,3 +1,4 @@
 class bitlancerorg::role::php_app_server inherits bitlancerorg::role {
-  include stringed::profile::apache_phpfpm
+  include stringed::profile::apache
+  include stringed::profile::phpfpm
 }
